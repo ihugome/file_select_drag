e00b543ef96b8ca9ec0acd9fb8dc2a0c8bbda46a
linlei <linlei@kingsoft.com>
2017-09-14 16:39:48 +0800

    route

--- vue-file-select-drag/src/router/index.js
+++ vue-file-select-drag/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import List from '@/components/List'
+import Drag from '@/components/Drag'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
             path: '/',
             name: 'List',
             component: List
+        },
+        {
+            path: '/Drag',
+            name: 'Drag',
+            component: Drag
         }
     ]
 })
